2b26e0807b1b04d61f494e895c6d6c08ee316111
Daniil Kolyasnikov <kolyasnikov@secret.su>
2020-11-20 15:19:47 +0500

    другая анимация

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:ascii_clock/ascii_display/ascii_display.dart';
 import 'package:ascii_clock/ascii_display/ascii_surfaces.dart';
 
@@ -8,6 +10,10 @@ main() {
 
   display.addSurface(
       StaticSurface.initFromFile('./templates/hotel.txt', 17, 2, 33, 16));
+  display.addSurface(AnimdatedSurface(40, 8, 16, 10, (int step) {
+    return List.generate(
+        16 * 10, (index) => Random().nextInt(6) == 1 ? "`" : " ").join();
+  }, stepCount: 5));
 
   display.addSurface(StaticSurface.initFromFile(
       './templates/cloud_with_moon.txt', 39, 3, 10, 17));
@@ -22,24 +28,41 @@ main() {
     result += date.second.toString().padLeft(2, "0");
     return result;
   }));
+  display.addSurface(AnimdatedSurface(3, 2, 1, 1, (int step) {
+    ;
+    if (step == 0) return "|";
+    if (step == 1) return "\\";
+    if (step == 2) return "-";
+    if (step == 3) return "/";
+    if (step == 4) return "|";
+    if (step == 5) return "\\";
+    if (step == 6) return "-";
+    if (step == 7) return "/";
+    return " ";
+  }, stepCount: 7));
 
-  display.addSurface(AnimdatedSurface(20, 16, 8, 1, (int step) {
-    if (step == 0) return "~~~~~~  ";
-    if (step == 1) return " ~~~~~~ ";
-    if (step == 2) return "  ~~~~~~";
-    if (step == 3) return " ~~~~~~ ";
-    if (step == 4) return "~~~~~~  ";
-    return "";
-  }, stepCount: 4));
-
-  display.addSurface(AnimdatedSurface(22, 15, 8, 1, (int step) {
-    if (step == 4) return "  ~~~~~~";
-    if (step == 3) return " ~~~~~~ ";
-    if (step == 2) return "~~~~~~  ";
-    if (step == 1) return " ~~~~~~ ";
-    if (step == 0) return "  ~~~~~~";
-    return "";
-  }, stepCount: 4));
+  display.addSurface(AnimdatedSurface(22, 15, 8, 2, (int step) {
+    String preresult;
+    if (step == 5) {
+      preresult = "  ~~~~~~";
+    }
+    if (step == 4) {
+      preresult = " ~~~~~~ ";
+    }
+    if (step == 3) {
+      preresult = "~~~~~~  ";
+    }
+    if (step == 2) {
+      preresult = "~~~~~~  ";
+    }
+    if (step == 1) {
+      preresult = " ~~~~~~ ";
+    }
+    if (step == 0) {
+      preresult = "  ~~~~~~";
+    }
+    return preresult + preresult.split("").reversed.join();
+  }, stepCount: 5));
 
   display.run();
 }
